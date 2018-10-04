d38ff4b1a50bf31cdb31c679cc6e1efc616bd9f6
hanse174 <hanse174@JB202-L21-W-D>
2018-10-04 14:55:18 -0400

    Began implementation of ReadAll method in CsvDataService class.

---
+++ Demo_FileIO_NTier.Starter/DataAccessLayer/CsvDataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo_FileIO_NTier.Models;
+
+namespace Demo_FileIO_NTier.Starter.DataAccessLayer
+{
+    class CsvDataService : IDataService
+    {
+        private string _dataFilePath;
+
+        public CsvDataService()
+        {
+            _dataFilePath = DataSettings.dataFilePath;
+        }
+
+        public IEnumerable<Character> ReadAll()
+        {
+            List<string> characters
+        }
+
+        public void WriteAll(IEnumerable<Character> characters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
